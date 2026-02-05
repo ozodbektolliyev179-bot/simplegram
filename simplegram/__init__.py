c9a78fc97337caa61c7009f73b391d8312da7ade
djumanov <djumanovdev@gmail.com>
2026-02-05 19:04:24 +0500

    dict to obj

---
+++ simplegram/__init__.py
@@ -0,0 +1,8 @@
+from .types import Update
+
+a = 3
+
+__version__ = '1.0.0'
+__all__ = [
+    'Update',
+]
---
+++ simplegram/types.py
@@ -0,0 +1,30 @@
+class User:
+
+    def __init__(self, data: dict):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data.get('last_name')
+        self.username = data.get('username')
+
+
+class Chat:
+
+    def __init__(self, data: dict):
+        self.id = data['id']
+        self.type = data['type']
+
+
+class Message:
+
+    def __init__(self, data: dict):
+        self.message_id = data['message_id']
+        self.from_user = User(data['from']) if data.get('from') else None
+        self.chat = Chat(data['chat']) if data.get('chat') else None
+        self.text = data.get('text')
+
+
+class Update:
+
+    def __init__(self, data: dict):
+        self.update_id = data['update_id']
+        self.message = Message(data['message']) if data.get('message') else None
